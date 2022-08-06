000b57a59f09c71f34bb34c9004c8d43d3210904
Douglas Peron <peron930@gmail.com>
2022-08-06 13:40:13 -0300

    ajuste de chamada API
    ajuste da chamada da API usando Axios.JS e localização da data para PT

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,67 +1,54 @@
-function fetchJson(url, options) {
-  return fetch(url, options).then((r) => {
-    if (r.ok) {
-      return r.json();
-    } else {
-      throw new Error(r.statusText);
-    }
-  });
-}
-const urlMatriz = "https://api.covid19api.com/summary";
+(async () => {
+  let response = await axios.get("https://api.covid19api.com/summary");
+
+  getData(response.data.Global);
+  loadPizza(response.data.Global);
+})();
 const urlValores =
   "https://api.covid19api.com/total/dayone/country/brazil/status/deaths";
 
-async function init() {
-  [summary] = await Promise.all([fetchJson(`${urlMatriz}`)]);
-  var confirmados = 0;
-  var recuperados = 0;
-  var mortes = 0;
-  confirmados = summary.Global.TotalConfirmed;
-  recuperados = summary.Global.TotalRecovered;
-  mortes = summary.Global.TotalDeaths;
-  getData(confirmados, recuperados, mortes);
-}
-init();
+function loadPizza(json) {
+  let totalData = [json.NewConfirmed, json.NewDeaths, json.NewRecovered];
 
-let gpizza = new Chart(document.getElementById("pizza"), {
-  type: "pie",
-  data: {
-    labels: ["Confirmados", "Recuperados", "Mortes"],
-    datasets: [
-      {
-        data: [],
-        backgroundColor: [
-          "rgb(255, 99, 132)",
-          "rgb(54, 162, 235)",
-          "rgb(255, 205, 86)",
-        ],
-      },
-    ],
-  },
-  options: {
-    response: true,
-    plugin: {
-      legend: {
-        position: "top",
-      },
-      title: {
-        text: "Casos Globais",
+  let gpizza = new Chart(document.getElementById("pizza"), {
+    type: "pie",
+    data: {
+      labels: ["Confirmados", "Recuperados", "Mortes"],
+      datasets: [
+        {
+          data: totalData,
+          backgroundColor: [
+            "rgb(255, 99, 132)",
+            "rgb(54, 162, 235)",
+            "rgb(255, 205, 86)",
+          ],
+        },
+      ],
+    },
+    options: {
+      response: true,
+      plugin: {
+        legend: {
+          position: "top",
+        },
+        title: {
+          text: "Casos Globais",
+        },
       },
     },
-  },
-});
-//função lindona add item.
-function getData(confirmados, recuperados, mortes) {
-  gpizza.data.datasets[0].data.push(confirmados);
-  gpizza.data.datasets[0].data.push(recuperados);
-  gpizza.data.datasets[0].data.push(mortes);
-
-  var conf = confirmados;
-  document.getElementById("confirmed").innerHTML = conf;
-  var morte = mortes;
-  document.getElementById("death").innerHTML = morte;
-  var recup = recuperados;
-  document.getElementById("recovered").innerHTML = recup;
-  gpizza.update();
+  });
 }
+
+//função lindona add item.
+function getData(json) {
+  document.getElementById("confirmed").innerHTML =
+    json.TotalConfirmed.toLocaleString("PT");
+  document.getElementById("death").innerHTML =
+    json.TotalDeaths.toLocaleString("PT");
+  document.getElementById("recovered").innerHTML =
+    json.TotalRecovered.toLocaleString("PT");
+
+  document.getElementById("date").innerHTML = json.Date;
+}
+
 function valorFiltrado() {}
